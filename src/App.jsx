50fbb93c3d7d84f6c7e781a5f6fd4d036cbd3322
Asish <gitasish1212@gmail.com>
2025-08-30 10:42:52 +0530

    write the basic code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hii!! Everyone I am Asish Kumar Bera founder of Crix company.</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
